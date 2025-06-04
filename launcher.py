d680ee1336457c4d0fa499240a26f2f0eabb8bc5
PaoloRos <184849342+PaoloRos@users.noreply.github.com>
2025-06-04 13:32:32 +0200

    m->

--- launcher.py
+++ launcher.py
@@ -19,34 +19,38 @@ else:
 # 2. Execute the software
 WARMUP = 3
 N = 50  # exe. repetitions
-matrix_size = sys.argv[1]
+matrix_size = 32#sys.argv[1]
 args = [matrix_size, "4", "1000", "50"]
 
 print("Execution:")
 subprocess.run( ["./parallel"] + args)
 
-#for i in range(N + WARMUP):
-#    print(f"Execution {i+1}/{N}:")
-#    subprocess.run(["./par_block"] + args)
+for i in range(6):
+    for i in range(N + WARMUP):
+        print(f"Execution {i+1}/{N}:")
+        subprocess.run(["./par_block"] + args)
 
-# 3. Read data
-time = []
-with open("par_time.csv") as my_file:
-    for idx, line in enumerate(my_file):
-        if idx < WARMUP:
-            continue # Skip first values
-        try:
-            time.append(float(line.strip()))
-        except ValueError:
-            continue
+    # 3. Read data
+    time = []
+    with open("par_time.csv") as my_file:
+        for idx, line in enumerate(my_file):
+            if idx < WARMUP:
+                continue # Skip first values
+            try:
+                time.append(float(line.strip()))
+            except ValueError:
+                continue
 
-if not time:
-    print("No data founded.\n")
-    sys.exit(1)
+    if not time:
+        print("No data founded.\n")
+        sys.exit(1)
 
-# 4. Statistics
-media = np.mean(time)
-dev_std = np.std(time)
+    # 4. Statistics
+    media = np.mean(time)
+    dev_std = np.std(time)
 
-print(f"Mean on {idx + 1 - WARMUP} elements: {media:.6f} (s)")
-print(f"Standard deviation: {dev_std:.6f} (s)\n")
+    print(f"Mean on {idx + 1 - WARMUP} elements: {media:.6f} (s)")
+    print(f"Standard deviation: {dev_std:.6f} (s)\n")
+    print("\n--------\n")
+
+    args[0] = str( int(args[0]) * 2 )
