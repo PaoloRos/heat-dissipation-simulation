ca807eda16735d6fd859d86f54bd490b5d6a5bb5
PaoloRos <184849342+PaoloRos@users.noreply.github.com>
2025-06-04 13:07:12 +0200

    m->

--- par_block.cc
+++ par_block.cc
@@ -115,12 +115,12 @@ int main(const int argc, const char **argv)
         blocks_per_col = (N + MAX_SIZE - 1) / MAX_SIZE;
         B_row = B_col = MAX_SIZE;
     }
-    else
-    {
-        blocks_per_row = 1 << (int)(log2(THD) / 2); // 2^(floor(log2(THD)/2))
-        blocks_per_col = THD / blocks_per_row;
-        B_row = N / blocks_per_row, B_col = N / blocks_per_col;
-    }
+    //else
+    //{
+    //    blocks_per_row = 1 << (int)(log2(THD) / 2); // 2^(floor(log2(THD)/2))
+    //    blocks_per_col = THD / blocks_per_row;
+    //    B_row = N / blocks_per_row, B_col = N / blocks_per_col;
+    //}
 
     const short total_blocks = blocks_per_row * blocks_per_col;
 
